29f06f691552d0b32b9143e890909ff8c11c2772
Daniel Grittner <danielgrittner@web.de>
2019-11-24 06:23:50 +0100

    add strings to web app

--- frontend-pos/src/components/AttentionCard.js
+++ frontend-pos/src/components/AttentionCard.js
@@ -1,35 +1,65 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Grid, Card, Paper, ButtonBase } from '@material-ui/core';
-import alertTriangle from '@material-ui/icons/PriorityHigh';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Grid, Card, Paper, ButtonBase } from "@material-ui/core";
+import alertTriangle from "@material-ui/icons/PriorityHigh";
 import UseAnimations from "react-useanimations";
 
-
 const STRING_MAP = {
-  "Asthma": {
-    
+  Asthma: {
+    actionItems: ["Asthma"]
+  },
+  Diabetes: {
+    actionItems: [
+      "In case of fatigue, check blood sugar",
+      "Provide customer with information on bread units"
+    ]
+  },
+  Epilepsy: {
+    actionItems: [
+      "Make sure that the passenger sleeps enough to reduce the probability of a seizure",
+      "Check the onboard medical kit for anti-seizure medication"
+    ]
+  },
+  Thrombosis: {
+    actionItems: [
+      "Encourage the customer to move around as often as possible",
+      "In case, notify the customer to avoid crossing legs"
+    ]
+  },
+  Autism: {
+    actionItems: [
+      "Provide separate sensory-friendly waiting room and seating",
+      "Offer contingency plans for possible flight delays"
+    ]
+  },
+  Parkinson: {
+    actionItems: [
+      "Offer early seating on board and to allow getting comfortable",
+      "Help customer to pick an aisle seat"
+    ]
   }
 };
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
 
   container: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(12, 1fr)',
-    gridGap: theme.spacing(3),
+    display: "grid",
+    gridTemplateColumns: "repeat(12, 1fr)",
+    gridGap: theme.spacing(3)
   },
 
   paper: {
     padding: theme.spacing(2),
-    margin: 'auto',
+    margin: "auto",
     maxWidth: 500,
+    minWidth: 450,
     maxHeight: 200,
     marginBottom: theme.spacing(1),
     backgroundColor: "#C9D0E7",
-    boxShadow: '0 3px 5px 2px rgba(24,24,24, .3)'
+    boxShadow: "0 3px 5px 2px rgba(24,24,24, .3)"
   },
 
   image: {
@@ -38,87 +68,78 @@ const useStyles = makeStyles(theme => ({
     borderRadius: 50
   },
   img: {
-    margin: 'auto',
-    display: 'block',
+    margin: "auto",
+    display: "block",
     maxWidth: 100,
     maxHeight: 100,
     borderRadius: 50
-  },
+  }
 }));
+
+const renderActionItems = disabilities => {
+  const actionItems = [];
+
+  for (let i = 0; i < disabilities.length; i++) {
+    for (let j = 0; j < STRING_MAP[disabilities[i]].actionItems.length; j++) {
+      actionItems.push(<li>{STRING_MAP[disabilities[i]].actionItems[j]}</li>);
+    }
+  }
+
+  return actionItems;
+};
 
 const AttentionCard = ({ beaconUserData }) => {
   const classes = useStyles();
-  const disability = "Diabetes";
+  const { name, disabilities } = beaconUserData;
+
+  if (!disabilities || !disabilities.length) {
+    return null;
+  }
 
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={2} >
+        <Grid container wrap="nowrap" spacing={2}>
           <Grid item xs={12} sm container>
             <Grid>
-              <UseAnimations animationKey="alertOctagon" size={56} style={{ padding: 5, display: 'flex', alignItems: 'center', justifyContent: 'center', }} />
+              <UseAnimations
+                animationKey="alertOctagon"
+                size={56}
+                style={{
+                  padding: 5,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}
+              />
             </Grid>
-            <Grid item xs container direction="column" justify="flex-start" spacing={2}>
+            <Grid
+              item
+              xs
+              container
+              direction="column"
+              justify="flex-start"
+              spacing={2}
+            >
               <Grid item xs>
                 <Typography gutterBottom variant="subtitle1">
-                  First and last name
+                  {name}
                 </Typography>
                 <Typography variant="body2" color="textSecondary">
-                  <li>In case of fatigue, check blood sugar</li>
-                  <li>Provide customer with information on bread units</li>
-                  <li>Check well-being every 2h, hold sugary food ready</li>
+                  {renderActionItems(disabilities)}
                 </Typography>
               </Grid>
-
             </Grid>
             <Grid item>
-              <Typography fontStyle="italic" variant="subtitle1">Disability</Typography>
+              <Typography fontStyle="italic" variant="subtitle1">
+                {disabilities.join(", ")}
+              </Typography>
             </Grid>
           </Grid>
         </Grid>
       </Paper>
     </div>
   );
-}
-
-
-  /*
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    height: 140,
-    width: 100,
-  },
-  control: {
-    padding: theme.spacing(2),
-  },
-}));
-
-  return (
-    <Grid container>
-      <Grid item xs={10}>
-        <Typography>{disability}</Typography>
-        <Grid container justify="center" spacing={spacing}>
-          {[0, 1, 2].map(value => (
-            <Grid key={value} item>
-              <Paper className={classes.paper} />
-            </Grid>
-          ))}
-        </Grid>
-      </Grid>
-      <Grid item xs={10}>
-        <Typography>Symptoms</Typography>
-      </Grid>
-      <Grid item xs={10}>
-        <Typography>Action Steps</Typography>
-      </Grid>
-    </Grid>
-  );
-}
-
-
-*/
+};
 
 export default AttentionCard;
--- frontend-pos/src/components/Home.js
+++ frontend-pos/src/components/Home.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Typography, Container, Grid,
-} from '@material-ui/core';
-import AttentionCard from './AttentionCard';
+import React, { useState, useEffect } from "react";
+import { Typography, Container, Grid } from "@material-ui/core";
+import AttentionCard from "./AttentionCard";
 
 const BEACON_ID = "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0";
 
@@ -17,52 +15,64 @@ const useStyles = makeStyles(theme => ({
   },
 */
 
-//(AttentionCard) && 
+//(AttentionCard) &&
 const Home = () => {
   const [websocket, setWebsocket] = useState(null);
   const [userData, setUserData] = useState(null);
 
   const connect = () => {
-    const ws = new WebSocket(`ws://192.168.8.113:8000/beacon-queue/${BEACON_ID}`);
+    const ws = new WebSocket(
+      `ws://192.168.8.113:8000/beacon-queue/${BEACON_ID}`
+    );
 
     ws.onopen = () => {
       setWebsocket(ws);
-    }
+    };
 
     ws.onmessage = e => {
       const beaconUserData = JSON.parse(e.data);
       const realData = beaconUserData.data;
       setUserData(realData);
       console.log(`USER_DATA: ${JSON.stringify(realData)}`); // FIXME
-    }
+    };
 
     ws.onclose = () => {
       setWebsocket(null);
       setUserData(null);
-    }
-  }
+    };
+  };
 
   useEffect(() => {
     if (!websocket) {
       connect();
     }
   });
-  
+
+  // console.log(`USER_DATA: ${JSON.stringify(userData)}`);
+
   return (
-    <Grid container style={{width: "100%", minHeight: 620, backgroundColor: "blue"}}>
-      {userData && (
-        <Grid item alignItems={"flex-start"} style={{marginTop: "auto", marginLeft: "1%", marginBottom: "1%"}}>
+    <Grid
+      container
+      style={{ width: "100%", minHeight: 620, backgroundColor: "blue" }}
+    >
+      {userData && userData !== {} && (
+        <Grid
+          item
+          alignItems={"flex-start"}
+          style={{ marginTop: "auto", marginLeft: "1%", marginBottom: "1%" }}
+        >
           <AttentionCard beaconUserData={userData} />
         </Grid>
       )}
-      <Grid item alignItems={"flex-end"} style={{backgroundPosition: 'center', backgroundSize: 'cover',}}>
-        <img src ="https://ak0.picdn.net/shutterstock/videos/1031472830/thumb/1.jpg?ip=x480" />
+      <Grid
+        item
+        alignItems={"flex-end"}
+        style={{ backgroundPosition: "center", backgroundSize: "cover" }}
+      >
+        <img src="https://ak0.picdn.net/shutterstock/videos/1031472830/thumb/1.jpg?ip=x480" />
       </Grid>
     </Grid>
-    );
-}
-
+  );
+};
 
 export default Home;
-
-
